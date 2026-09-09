4e7d66bf44df906ef3023867b5a065a9a667b1cd
GIOVANE FELIPE FREITAS DA SILVA <162.609364@universidadesenaicimatec.edu.br>
2026-09-09 16:40:21 -0300

    Segundo commit

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Menu } from '../../componentes/menu/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Menu],
+  imports: [Menu, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
